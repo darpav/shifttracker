d85c1a956620a0ae32104ccfc70bbd9469b1888d
drc <dadopavlekovic@gmail.com>
2025-03-04 22:01:48 +0100

    delete fixed

--- src/main/java/com/hita/shifttracker/controller/EmployeeController.java
+++ src/main/java/com/hita/shifttracker/controller/EmployeeController.java
@@ -139,7 +139,7 @@ public class EmployeeController {
 
         AppUserDTO appUser = (AppUserDTO) session.getAttribute("appUser");
 
-        //workingTimeService.deleteWorkingTimeById(appUser.getId(), workingTimeToDelete);
+        workingTimeService.deleteWorkingTimeById(workingTimeToDelete);
 
         return "redirect:/employee/workhour/list";
     }

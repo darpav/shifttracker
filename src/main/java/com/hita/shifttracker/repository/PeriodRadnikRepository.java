81de72141e693379173a76889e3173b398ed677c
drc <dadopavlekovic@gmail.com>
2025-03-05 23:32:53 +0100

    period_radnik

--- src/main/java/com/hita/shifttracker/repository/PeriodRadnikRepository.java
+++ src/main/java/com/hita/shifttracker/repository/PeriodRadnikRepository.java
@@ -1,7 +1,10 @@
 package com.hita.shifttracker.repository;
 
+import com.hita.shifttracker.model.PeriodRadnik;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class PeriodRadnikRepository {
@@ -12,5 +15,24 @@ public class PeriodRadnikRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    // find all
+    public List<PeriodRadnik> findAll() {
+        String sql = "SELECT * FROM period_radnik";
 
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            PeriodRadnik periodRadnik = new PeriodRadnik();
+
+            periodRadnik.setId(rs.getInt("id"));
+            periodRadnik.setAppUserId(rs.getInt("app_user_id"));
+            periodRadnik.setPeriodId(rs.getInt("period_id"));
+            periodRadnik.setStatus(rs.getString("status"));
+            periodRadnik.setInsertedAt(rs.getTimestamp("inserted_at"));
+            periodRadnik.setUpdatedAt(rs.getTimestamp("updated_at"));
+            periodRadnik.setUidInsUpd(rs.getInt("uid_ins_upd"));
+            periodRadnik.setYear(rs.getInt("year"));
+            periodRadnik.setMonth(rs.getInt("month"));
+
+            return periodRadnik;
+        });
+    }
 }

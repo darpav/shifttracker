2344cf452ec5d5ed420483f349bb87e2d8ce53c4
drc <dadopavlekovic@gmail.com>
2025-03-09 05:34:51 +0100

    all_combined

--- src/main/java/com/hita/shifttracker/repository/LeaveRecordRepository.java
+++ src/main/java/com/hita/shifttracker/repository/LeaveRecordRepository.java
@@ -4,6 +4,8 @@ import com.hita.shifttracker.model.LeaveRecord;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public class LeaveRecordRepository {
 
@@ -22,5 +24,19 @@ public class LeaveRecordRepository {
                 leaveRecord.getTotalDays(), leaveRecord.getTotalHours(), leaveRecord.getUidInsUpd());
     }
 
+    // find by app user id and date from
+
+    // exist by app user id and date from
+    public boolean existsByAppUserIdAndDateFrom(int appUserId, LocalDate dateFrom) {
+        String sql = "SELECT COUNT(*) FROM leave_record WHERE app_user_id = ? AND date_from = ?";
+        return jdbcTemplate.queryForObject(sql, new Object[]{appUserId, dateFrom}, Integer.class) > 0;
+    }
+
+    // exists by app user id and worktype other id, and date from in between date from and date to, and date to in between date from and date to
+    public boolean overlapRecord(int appUserId, int workTypeOtherId, LocalDate dateFrom, LocalDate dateTo) {
+        String sql = "SELECT COUNT(*) FROM leave_record WHERE app_user_id = ? AND work_type_other_id = ? AND " +
+                "(date_from BETWEEN ? AND ? OR date_to BETWEEN ? AND ?)";
+        return jdbcTemplate.queryForObject(sql, new Object[]{appUserId, workTypeOtherId, dateFrom, dateTo, dateFrom, dateTo}, Integer.class) > 0;
+    }
 
 }
